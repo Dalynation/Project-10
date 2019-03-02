0e0bbbfc0a4390b08cfe37c904a2636cb7a9d625
Dalynation <42184269+Dalynation@users.noreply.github.com>
2019-03-01 23:54:59 -0500

    Add files via upload
    I added the rest of the components of the site excluding the error components just to get an overall visual. HTML mockups had to be converted to jsx.

--- full_stack_app_with_react_and_a_rest_api-v1 (3)/client/src/App.js
+++ full_stack_app_with_react_and_a_rest_api-v1 (3)/client/src/App.js
@@ -6,8 +6,10 @@ import "./styles/global.css";
 
 //import the react components
 import Courses from "./components/Courses";
-
-
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import CourseDetail from "./components/CourseDetail";
+import CreateCourse from "./components/CreateCourse";
 
  
   
@@ -22,6 +24,10 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path = "/" component={Courses} />
+            <Route exact path = "/sign-up" component={Signup} />
+            <Route exact path = "/sign-in" component={Signin} />
+            <Route exact path = "/course-detail" component={CourseDetail} />
+            <Route exact path = "/create-course" component={CreateCourse} />
           </Switch>
         </div>
 
